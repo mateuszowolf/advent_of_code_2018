be69d2f8514bcb9c5afbfffd726f1a34d913137a
mateuszowolf <mateusz.o.wolf@outlook.com>
2018-12-04 23:59:45 +0000

    completed day 3

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,31 +1,30 @@
 import numpy as np
 import os
+import re
 
 FILE_PATH = os.path.join('day_3', 'input.txt')
 
+with open(FILE_PATH) as f:
+    claims = []
+    for r in f.readlines():
+        r = re.split('[^0-9]+', r[1:].strip())
+        claims.append([int(d) for d in r])
+
+fabric = np.zeros((1000,1000))
+
 def part_1():
-    fabric = np.zeros((1000,1000))
 
-    with open(FILE_PATH, 'r') as f:
-        for claim in f:
-            # claim = "#1 @ 1,3: 4x5\n"
-            claim = claim.strip().split()
-            x, y = claim[2].strip(':').split(',')
-            x, y = int(x), int(y)
-            width, height = claim[3].split('x')
-            width, height = int(width), int(height)
-
-            for row in range(y, y+height):
-                for column in range(x, x+ width):
-                    fabric[row][column] += 1
-
+    for claim in claims:
+        id, x, y, width, height = claim
+        fabric[x:x+width, y:y+height] += 1
     print(len(fabric[fabric>1]))
 
+def part_2():
+    for claim in claims:
+        id, x, y, width, height = claim
+        if np.all(fabric[x:x+width, y:y+height] == 1):
+            print(id)
+
 if __name__ == '__main__':
-    choice = int(input('Which part woud you like to run: \n'))
-    if choice == 1:
         part_1()
-    # elif choice == 2:
-    #     part_2()
-    else:
-        print('only 1 or 2 are covered')
+        part_2()
